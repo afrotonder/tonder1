47097e8899a5dbfe9862d81fe7cebe851168e0eb
afrotonder <o.rosado18@gmail.com>
2022-01-04 10:55:10 -0400

    Add resume view to 3rd tab.

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DocumentViewerOptions } from '@awesome-cordova-plugins/document-viewer';
+import { DocumentViewer } from '@awesome-cordova-plugins/document-viewer/ngx';
+import { FileOpener } from '@awesome-cordova-plugins/file-opener/ngx';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +10,28 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(private document: DocumentViewer,
+              private fileOpener: FileOpener) {
 
+
+  }
+
+  ngAfterViewInit()	{
+    this.renderResume()
+  }
+
+  renderResume() {
+
+
+    this.fileOpener.open('../../assets/images/resume_dec2021.pdf', 'application/pdf')
+  .then(() => console.log('File is opened'))
+  .catch(e => console.log('Error opening file', e));
+
+
+    // const options: DocumentViewerOptions = {
+    //   title: 'My PDF'
+    // }
+
+    // this.document.viewDocument('../../assets/images/resume_dec2021.pdf', 'application/pdf', options)
+  }
 }
