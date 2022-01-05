f39872806ac922042827985e1e15bdc27001f982
afrotonder <o.rosado18@gmail.com>
2022-01-05 16:33:46 -0400

    Remove unused about page from routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,7 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
-    path: 'about',
-    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
-  }
-
+  },
 ];
 @NgModule({
   imports: [
