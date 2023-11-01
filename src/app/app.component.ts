2d61c5348d8ebd2daea326d7ab82e89c807e9207
afrotonder <o.rosado18@gmail.com>
2023-11-01 14:10:23 -0400

    Add new data in angular format. Remove unused functions. Add dynamic element templates for job positions and personal info. Replace all inline styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { initializeApp } from "firebase/app";
-import { environment } from 'src/environments/environment.prod';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -15,10 +14,14 @@ export class AppComponent {
    sm_uname: string;
    handle: string;
    title: string;
+   titles: Array<any>;
    slogan: string;
    job1: string;
    job2: string;
+   job3: string
    footer: string;
+   contactIcons: Array<any>;
+   jobList: Array<any>;
 
   constructor() {
 
@@ -26,126 +29,101 @@ export class AppComponent {
     this.lastNames = "Rosado Ramírez"
     this.sm_uname = "O. Rosado Ramírez";
     this.handle = "'afrotonder'";
-    this.title = "Full Stack Developer ⚡ Mobile Developer"
+    this.title = "⚡Full Stack Developer ⚡Mobile Developer ⚡Tech Consultant"
     this.slogan = "Full-time problem solver";
     this.job1 = "TuChequera.com";
     this.job2 = "APS Health";
     this.footer = `afrotonder © ${new Date().getFullYear().toString()}`;
-    ;
+
+    this.titles = [
+      "⚡Full Stack Developer",
+      "⚡Mobile Developer",
+      "⚡Tech Consultant"
+    ]
+
+    this.jobList = [
+      {
+        title: "Full Stack Developer",
+        employer: "TuChequera.com",
+        url: "https://tuchequera.com/"
+      },
+      {
+        title: "BI Developer",
+        employer: "APS Health",
+        url: "https://apshealth.com/"
+      },
+      {
+        title: "Web3 Consultant",
+        employer: "Thunder1 Consulting",
+        url: "afrotonder.com"
+      },
+    ]
+
+    this.contactIcons = [
+      {
+        name: "lin",
+        url: "https://www.linkedin.com/in/afrotonder/",
+        title: "afrotonder@LinkedIn",
+        alt: "LinkedIn Icon",
+        source: "../assets/images/in-icon.png"
+      },
+      {
+        name: "gmail",
+        url: "mailto:o.rosado18@gmail.com",
+        title: "o.rosado18@gmail.com",
+        alt: "Gmail Icon",
+        source: "../assets/images/gmail.png"
+      },
+      {
+        name: "github",
+        url: "https://github.com/afrotonder",
+        title: "afrotonder@GitHub",
+        alt: "Github Octocat Logo",
+        source: "../assets/images/github.png"
+      },
+    
+
+      {
+        name: "Medium",
+        url: "https://medium.com/@afrotonder",
+        title: "afrotonder@Medium",
+        alt: "Medium Icon",
+        source: "../assets/images/medium.png"
+      },
+
+      {
+        name: "linktree",
+        url: "https://linktr.ee/afrotonder",
+        title: "afrotonder@LinkTree",
+        alt: "LinkTree Icon",
+        source: "../assets/images/linktree.png" 
+      },
+      // {
+      //   name: "chess",
+      //   url: "https://www.chess.com/member/afrotonder",
+      //   title: "afrotonder@Chess.com",
+      //   alt: "Chess.com Icon",
+      //   source: "https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/SamCopeland/phpmeXx6V.png"
+      // },
+      // {
+      //   name: "os",
+      //   url: "https://opensea.io/afrotonder",
+      //   title: "afrotonder@OpenSea.io",
+      //   alt: "OpenSea Icon",
+      //   source: "https://opensea.io/static/images/logos/opensea.svg"
+      // },
+      // {
+      //   name: "twt",
+      //   url: "https://twitter.com/afrotonder",
+      //   title: "afrotonder@Twitter",
+      //   alt: "Twitter Icon",
+      //   source: "https://icons-for-free.com/iconfiles/png/512/identity+logo+new+twitter+icon-1320161424699291885.png"
+      // },
+      
+    ];
 
   }
 
-  ngAfterViewInit()	{
-    this.init()
-  }
-
-  init() {
-    // ---------- SET AVATAR DATA ----------
-const sm_uname = "O. Rosado Ramírez";
-const handle = "afrotonder";
-const slogan = "Full-time problem solver";
-const job1 = "TuChequera.com";
-const job2 = "APS Health";
-// const job3 = "Free" ;
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA1Bi9Dd9pSPyqeolk7AJhQAQD1tEvTGaA",
-  authDomain: "tonder1-bd704.firebaseapp.com",
-  projectId: "tonder1-bd704",
-  storageBucket: "tonder1-bd704.appspot.com",
-  messagingSenderId: "346941604927",
-  appId: environment.kawaki
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-
-// ---------- ICON DATA----------
-let imgs = [
-  {
-    name: "lin",
-    url: "https://www.linkedin.com/in/afrotonder/",
-    title: "afrotonder@LinkedIn",
-    alt: "LinkedIn Icon",
-    source: "https://brandlogos.net/wp-content/uploads/2016/06/linkedin-logo-512x512.png"
-  },
-  // {
-  //   name: "grm",
-  //   url: "https://angular-g2qjyv.stackblitz.io/",
-  //   title: "TheGripRepo",
-  //   alt: "Rob Zombies Halloween 2018 Pumpkin Icon",
-  //   source:  "https://trickortreatstudios.com/media/catalog/product/cache/1da4909b8e3ea5eea17a9fb4c6e4a516/h/a/halloween2018-enamelpin_1.png"
-  // },
-  {
-    name: "github",
-    url: "https://github.com/afrotonder",
-    title: "afrotonder@GitHub",
-    alt: "Github Octocat Logo",
-    source: "https://github.githubassets.com/images/modules/logos_page/Octocat.png"
-  },
-  {
-    name: "linktree",
-    url: "https://linktr.ee/afrotonder",
-    title: "afrotonder@LinkTree",
-    alt: "LinkTree Icon",
-    source: "../assets/images/linktree.png" 
-  },
-  {
-    name: "gmail",
-    url: "mailto:o.rosado18@gmail.com",
-    title: "o.rosado18@gmail.com",
-    alt: "Gmail Icon",
-    source: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Gmail_Icon_%282013-2020%29.svg/2048px-Gmail_Icon_%282013-2020%29.svg.png"
-  },
-  // {
-  //   name: "chess",
-  //   url: "https://www.chess.com/member/afrotonder",
-  //   title: "afrotonder@Chess.com",
-  //   alt: "Chess.com Icon",
-  //   source: "https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/SamCopeland/phpmeXx6V.png"
-  // },
-  // {
-  //   name: "os",
-  //   url: "https://opensea.io/afrotonder",
-  //   title: "afrotonder@OpenSea.io",
-  //   alt: "OpenSea Icon",
-  //   source: "https://opensea.io/static/images/logos/opensea.svg"
-  // },
-  // {
-  //   name: "twt",
-  //   url: "https://twitter.com/afrotonder",
-  //   title: "afrotonder@Twitter",
-  //   alt: "Twitter Icon",
-  //   source: "https://icons-for-free.com/iconfiles/png/512/identity+logo+new+twitter+icon-1320161424699291885.png"
-  // },
-  
-];
-
-
-
-// set/ renderfooter
-for (let img of imgs) {
-  let elem = document.createElement("a");
-
-  elem.setAttribute("href", img.url);
-  elem.setAttribute("target", "_blank");
-  elem.setAttribute("class", "toolIcon");
-  elem.setAttribute("style", "width:50px;");
-
-  elem.innerHTML =
-    `<img src=` +
-    img.source +
-    `  title=` +
-    img.title +
-    ` alt=` +
-    img.alt +
-    `/>`;
-
-  document.getElementById("social-icons").appendChild(elem);
-}
-
-  }
 }
 
 
